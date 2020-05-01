8bba3d81fca3be51c0ed7af8287ca7ef7adb3c89
asiegf <asi@home.local>
2020-05-01 09:42:47 +0200

    init and mmap

---
+++ src/core.clj
@@ -0,0 +1,9 @@
+(ns core)
+
+(defmacro mmap
+  "Transforms a list of symbols into keywords and map them
+  with their bindings.
+  [foo  1, bar 2] => {:foo 1 :bar 2}"
+  [& symbols]
+  `(let [ks# (map keyword '~symbols)]
+     (zipmap ks# [~@symbols])))
